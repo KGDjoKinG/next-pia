e8d94c140c0057df0338126852a68b59ba700c49
KGDjoKinG Home <kgdjoking@gmail.com>
2024-04-15 23:18:41 +0800

    project initialized login page incomp

---
+++ src/app/ui/components/Icons/Logo.Icon.tsx
@@ -0,0 +1,16 @@
+
+"use client";
+
+import { FunctionComponent } from "react";
+
+interface IScreenLoader {
+    wText?: boolean
+}
+
+export const Logo: FunctionComponent<IScreenLoader> = ({ wText }) => {
+    return (
+        <div className="flex flex-col items-center w-max mx-auto">
+            
+        </div>
+    );
+};
---
+++ src/app/ui/components/custom/Logo/index.ts
@@ -0,0 +1,1 @@
+export { default as LogoX } from "./Logo.component"
